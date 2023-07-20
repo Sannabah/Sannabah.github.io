9ea318652290477a0643e7d0c798f83a38690a4a
sannabah <140054782+sannabah@users.noreply.github.com>
2023-07-20 07:55:17 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed= new typed('.text', {
+    strings: ["Front-end Developer" ,  "Software Engineer" , "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
